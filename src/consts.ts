ee23b1bd9ea253fe98743da83845bb5c87a984ba
kakaoseso <kakao.seso@gmail.com>
2026-01-21 06:27:34 +0000

    Enhance UI: Noto Sans KR, improved styles, fixed navigation, updated content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'HN 트렌드';
+export const SITE_DESCRIPTION = 'Hacker News의 주요 기술 트렌드와 인사이트를 한국어로 정리해 드립니다.';
